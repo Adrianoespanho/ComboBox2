d3962c0117c391333f35af8d63aff9fb5e5e58be
Adrianoespanho <112988600+Adrianoespanho@users.noreply.github.com>
2023-03-08 21:59:09 -0300

    tem que terminar falta um codigo

--- ComboBox2/Form1.Designer.cs
+++ ComboBox2/Form1.Designer.cs
@@ -30,8 +30,8 @@
         {
             this.label1 = new System.Windows.Forms.Label();
             this.textBox1 = new System.Windows.Forms.TextBox();
-            this.comboBox1 = new System.Windows.Forms.ComboBox();
-            this.button1 = new System.Windows.Forms.Button();
+            this.Seleciona = new System.Windows.Forms.ComboBox();
+            this.Remover = new System.Windows.Forms.Button();
             this.btnAdicionar = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
@@ -51,24 +51,27 @@
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(169, 20);
             this.textBox1.TabIndex = 1;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
-            // comboBox1
+            // Seleciona
             // 
-            this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(151, 173);
-            this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(169, 21);
-            this.comboBox1.TabIndex = 2;
+            this.Seleciona.FormattingEnabled = true;
+            this.Seleciona.Location = new System.Drawing.Point(151, 173);
+            this.Seleciona.Name = "Seleciona";
+            this.Seleciona.Size = new System.Drawing.Size(169, 21);
+            this.Seleciona.TabIndex = 2;
+            this.Seleciona.SelectedIndexChanged += new System.EventHandler(this.Seleciona_SelectedIndexChanged);
             // 
-            // button1
+            // Remover
             // 
-            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.Location = new System.Drawing.Point(141, 268);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 3;
-            this.button1.Text = "Remover";
-            this.button1.UseVisualStyleBackColor = true;
+            this.Remover.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Remover.Location = new System.Drawing.Point(141, 268);
+            this.Remover.Name = "Remover";
+            this.Remover.Size = new System.Drawing.Size(81, 23);
+            this.Remover.TabIndex = 3;
+            this.Remover.Text = "Remover";
+            this.Remover.UseVisualStyleBackColor = true;
+            this.Remover.Click += new System.EventHandler(this.Remover_Click);
             // 
             // btnAdicionar
             // 
@@ -87,12 +90,13 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(506, 365);
             this.Controls.Add(this.btnAdicionar);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.Remover);
+            this.Controls.Add(this.Seleciona);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.label1);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -102,8 +106,8 @@
 
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.ComboBox comboBox1;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ComboBox Seleciona;
+        private System.Windows.Forms.Button Remover;
         private System.Windows.Forms.Button btnAdicionar;
     }
 }
--- ComboBox2/Form1.cs
+++ ComboBox2/Form1.cs
@@ -19,11 +19,45 @@ namespace ComboBox2
 
         private void button2_Click(object sender, EventArgs e)
         {
-            if (TextBoxNome.textlength == 0 )
+            if (textBox1.TextLength == 0)
                 MessageBox.Show("Por favor digite algo", "aviso",MessageBoxButtons.OK,MessageBoxIcon.Error);
+            textBox1.Focus();
             {
-
+                Seleciona.Items.Add(textBox1.Text);
+                textBox1.Clear();
+                textBox1.Focus();
             }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+          // Seleciona.SelectedIndex = 0;
+        }
+
+        private void Remover_Click(object sender, EventArgs e)
+        {
+
+            if (Seleciona.SelectedIndex == 0) 
+            
+                MessageBox.Show("Por favor selecione um intem", "Aviso", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            {
+                Seleciona.Items.Remove(Seleciona.SelectedItem);
+                Seleciona.SelectedIndex = 0;
+            }
+            
+         
+          
+          
+        }
+
+        private void Seleciona_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
         }
     }
 }
